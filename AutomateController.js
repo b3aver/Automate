5e5711e06fe820675fcbe4f4a786a2666e0b4904
Paolo Sberna <pcsbe-cs@yahoo.it>
2017-11-14 19:30:49 +0100

    Create a standalone page with the import functionality (close #18)

--- AutomateController.js
+++ AutomateController.js
@@ -12,12 +12,8 @@ AutomateController = {
       AutomateController.refresh();
     });
 
-    jQuery('#export').click(function(){
-      AutomateController.exportToFile();
-    });
-
-    jQuery('#import').click(function(){
-      AutomateController.importFromFile();
+    jQuery('#options').click(function(){
+      browser.runtime.openOptionsPage();
     });
 
     jQuery('#addAutomationButton').click(function(){
@@ -32,33 +28,7 @@ AutomateController = {
     });
   },
 
-  exportToFile: function(){
-    // TODO: ask if save before
-    AutomateModel.load().then(function(automations){
-      // Convert object to a string.
-      let result = JSON.stringify(automations);
-
-      // now
-      let date = new Date();
-      let dateFormatted = '' + date.getFullYear()
-          + ('0' + (date.getMonth() + 1)).slice(-2)
-          + ('0' + date.getDate()).slice(-2)
-          + '_' + ('0' + date.getHours()).slice(-2)
-          + ('0' + date.getMinutes()).slice(-2);
-
-      // Save as file
-      var blob = new Blob([result], {type: "application/json;charset=utf-8"})
-      browser.downloads.download({
-        url: URL.createObjectURL(blob),
-        filename: 'automations_' + dateFormatted + '.json'
-      }).then(function (id) {
-        AutomateView.showMessage('Download started');
-      }, function (error) {
-        AutomateView.showError('Download failed');
-      })
-    });
-  },
-
+  // TODO: to remove
   importFromFile: function(){
     // TODO: ask confirmation because the automations will be overwritten
     let filename = jQuery('#importFileInput').val().replace('C:\\fakepath\\', '');
---
+++ options.js
@@ -0,0 +1,48 @@
+jQuery(function(){
+
+  AutomateModel.load().then(function(automations){
+    // Convert object to a string.
+    let result = JSON.stringify(automations);
+    jQuery('#importTextArea').val(result);
+  });
+
+  jQuery('#export').click(function(){
+    // TODO: ask if save before
+    AutomateModel.load().then(function(automations){
+      // Convert object to a string.
+      let result = JSON.stringify(automations);
+
+      // now
+      let date = new Date();
+      let dateFormatted = '' + date.getFullYear()
+          + ('0' + (date.getMonth() + 1)).slice(-2)
+          + ('0' + date.getDate()).slice(-2)
+          + '_' + ('0' + date.getHours()).slice(-2)
+          + ('0' + date.getMinutes()).slice(-2);
+
+      // Save as file
+      var blob = new Blob([result], {type: "application/json;charset=utf-8"})
+      browser.downloads.download({
+        url: URL.createObjectURL(blob),
+        filename: 'automations_' + dateFormatted + '.json'
+      }).then(function (id) {
+        AutomateView.showMessage('Download started');
+      }, function (error) {
+        AutomateView.showError('Download failed');
+      })
+    });
+  });
+
+  jQuery('#import').click(function(){
+    // TODO: ask confirmation because the automations will be overwritten
+
+    let importContent = jQuery('#importTextArea').val()
+    // TODO: check the parsification
+    AutomateModel.setAutomations(JSON.parse(importContent));
+    AutomateModel.save();
+
+    // Reloads the extension
+    browser.runtime.reload();
+  });
+
+});
