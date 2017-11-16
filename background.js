05f003ef3858326410ef6a37c51c417686fb8f9d
Paolo Sberna <pcsbe-cs@yahoo.it>
2017-11-16 18:30:15 +0100

    Fix open sidebar with browser action (close #26)

--- background.js
+++ background.js
@@ -1,16 +1,19 @@
 browser.browserAction.onClicked.addListener(() => {
-  browser.runtime.getBrowserInfo(function(info){
-    var mainVersion = info.version.substr(0,info.version.indexOf('.'));
-    if(mainVersion >= 57){
-      browser.sidebarAction.open();
-    } else if (mainVersion < 54) {
-      var pageDate = {
-        type: "detached_panel",
-        url: "automate.html",
-        width: 500,
-        height: 600
+  if (typeof browser.sidebarAction != 'undefined'){
+    browser.sidebarAction.open();
+  } else {
+    console.log('Sidebar open not supported');
+    browser.runtime.getBrowserInfo(function(info){
+      var mainVersion = info.version.substr(0,info.version.indexOf('.'));
+      if (mainVersion < 54) {
+        var pageDate = {
+          type: "detached_panel",
+          url: "automate.html",
+          width: 500,
+          height: 600
+        }
+        browser.windows.create(pageDate);
       }
-      browser.windows.create(pageDate);
-    }
-  });
+    });
+  }
 });
